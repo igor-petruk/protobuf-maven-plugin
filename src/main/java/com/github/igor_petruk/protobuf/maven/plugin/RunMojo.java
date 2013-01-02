1a3bd5bb77d41cf9da2507f83d214fde1aeec54e
Igor Petruk <igor.petrouk@gmail.com>
2013-01-02 21:39:37 +0200

    Modified license owner

--- src/main/java/com/github/igor_petruk/protobuf/maven/plugin/RunMojo.java
+++ src/main/java/com/github/igor_petruk/protobuf/maven/plugin/RunMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012, Igor Petruk
+ * Copyright 2012, by Yet another Protobuf Maven Plugin Developers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
